94decd441066dcc9fd2aa9fa5612aa5d8a489415
unknown <est.hguillenp0201@uml.edu.ni>
2025-02-15 14:38:45 -0600

    se agregaron configuracion en cyprees

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,27 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+
+import { loadEnv } from 'vite';
+
+
+
+const env = loadEnv("", process.cwd(), "VITE_");
 
 module.exports = defineConfig({
-  projectId: "fxatur",
-  e2e: {
-    baseUrl: 'http://localhost:5174',
+  projectId: "fxatur", // Usa el valor de entorno o un valor por defecto
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: 'src/**/*.cy.tsx',
   },
-})
+  e2e: {
+    baseUrl: env.VITE_CYPRESS_BASE_URL || "http://localhost:5174", // Usa el valor de entorno o un valor por defecto
+    supportFile: false,
+
+    // Record screenshots and videos
+    video: true,
+    videoCompression: 32,
+    screenshots: true,
+  },
+});
