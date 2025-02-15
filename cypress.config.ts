f054bd442691b251a7c625fa0993ad862ffca299
unknown <est.hguillenp0201@uml.edu.ni>
2025-02-15 14:32:12 -0600

    se cambio el puerto de cypressconfig

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "fxatur",
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:5174',
   },
 })
