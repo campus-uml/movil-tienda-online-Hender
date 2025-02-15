44c6d8c1de0a8c3e7adcb7b08add0dc35d25e5b4
unknown <est.hguillenp0201@uml.edu.ni>
2025-02-15 14:54:57 -0600

    se actualizo cypressconfig1

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from 'cypress';
 import { loadEnv } from 'vite';
 
-const env = loadEnv('', process.cwd(), 'VITE_');
+const env = loadEnv("", process.cwd(), "VITE_");
 
 export default defineConfig({
-  projectId: 'fxatur', // Usa el valor de entorno o un valor por defecto
+  projectId: "fxatur", // Usa el valor de entorno o un valor por defecto
   component: {
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
+      framework: "react",
+      bundler: "vite",
     },
-    specPattern: 'src/*/.cy.tsx',
+    specPattern: 'src/**/*.cy.tsx',
   },
   e2e: {
-    baseUrl: env.VITE_CYPRESS_BASE_URL || 'http://localhost:5174', // Usa el valor de entorno o un valor por defecto
+    baseUrl: env.VITE_CYPRESS_BASE_URL || "http://localhost:5174", // Usa el valor de entorno o un valor por defecto
     supportFile: false,
 
     // Record screenshots and videos
     video: true,
     videoCompression: 32,
-    screenshots: true,
   },
 });
